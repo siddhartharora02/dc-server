17cb0b0dd7d236650b281c7ec44ac1f2ade6be38
Siddharth <siddhartharora02@gmail.com>
2018-07-09 15:52:00 +0530

    changing to prod mongodb

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    // url : 'mongodb://18.220.200.64:27017/dc',
-    url : 'mongodb://localhost:27017/dc',
+    url : 'mongodb://139.59.59.197:27017/dc',
+    // url : 'mongodb://localhost:27017/dc',
     server_name: 'DC'
 };
