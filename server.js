73f46db864aea3cbcaf4849fdd1417a511516f2f
Siddharth <siddhartharora02@gmail.com>
2018-07-09 14:46:49 +0530

    changing port to 3001

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/', (req,res)=>{
 require('./app/routes/users.routes.js')(app);
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, ()=>{
     console.log("Server is running at http://localhost:"+ port)
 });
